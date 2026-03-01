434d6ffc5f91cf6295266fbd47257e6f51f212e1
Wesley <WesleyOlivier443@gmail.com>
2026-03-01 09:36:26 +0200

    04 Image generated

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       return false;
     }
 
-    setProjects((prev) => [newItem, ...prev]);
+    setProjects((prev) => [saved, ...prev]);
     // 👈 added
     navigate(`/visualizer/${newId}`, {
       state: {
@@ -99,7 +99,7 @@ export default function Home() {
           <div className="projects-grid">
             {projects.map(
               ({ id, name, renderedImage, sourceImage, timestamp }) => (
-                <div className="project-card group">
+                <div key={id} className="project-card group">
                   <div className="preview">
                     <img src={renderedImage || sourceImage} alt="project" />
                     <div className="badge">
